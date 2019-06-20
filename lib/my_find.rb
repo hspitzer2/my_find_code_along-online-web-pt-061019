354af68e528d4ed6d55a777e994349e72f18f636
Hillary Spitzer <hspitzer2@gmail.com>
2019-06-20 19:45:37 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,21 +3,8 @@ require 'pry'
 def my_find(collection)
   i = 0
   while i < collection.length
-    if yield(collection[i])
-      return collection[i].first
-    end
-    1 = += 1
+    return collection[i] if yield(collection[i])
   end
-end
-
-
-
-def my_find(collection)
-  i = 0
-  while i < collection.length
-    if yield(collection[i])
-      return collection[i]
-    end
-    i = i + 1
+    1 = += 1
   end
 end
